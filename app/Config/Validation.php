9f992c448a90577347fb9d3a5f0f0ae8283b981a
Jefferson0k <jefersoncovenas7@gmail.com>
2024-10-17 07:56:10 -0500

    Solucionado sucursales

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -41,4 +41,31 @@ class Validation extends BaseConfig
     // --------------------------------------------------------------------
     // Rules
     // --------------------------------------------------------------------
+    public $doseveSucursales = [
+        'nombre' => [
+            'rules' => 'required|min_length[2]|max_length[50]|alpha_space',
+            'errors' => [
+                'required' => 'El campo Nombre no acepta vacíos',
+                'min_length' => 'El campo Nombre debe tener mínimo 2 letras',
+                'max_length' => 'El campo Nombre no debe ser mayor de 50 letras',
+                'alpha_space' => 'El campo Nombre debe contener solo letras y espacios'
+            ]
+        ],
+        'direccion' => [
+            'rules' => 'required|min_length[2]|max_length[50]',
+            'errors' => [
+                'required' => 'El campo Dirección no acepta vacíos',
+                'min_length' => 'El campo Dirección debe tener mínimo 2 letras',
+                'max_length' => 'El campo Dirección no debe ser mayor de 50 letras'
+            ]
+        ],
+
+        'estado' => [
+            'rules' => 'required|integer',
+            'errors' => [
+                'required' => 'El campo Estado no acepta vacíos',
+                'integer' => 'El campo Estado debe contener solo números'
+            ]
+        ],
+    ];    
 }
